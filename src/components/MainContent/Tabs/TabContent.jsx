35ea29f2855e3570c2506baf96979ff3675d66e0
ztk1296 <zahrakhan3415@gmail.com>
2026-02-20 13:22:07 -0600

    deploy react project on github
    deploy configuration

--- src/components/MainContent/Tabs/TabContent.jsx
+++ src/components/MainContent/Tabs/TabContent.jsx
@@ -4,40 +4,51 @@ export const tabContent = [
            idInString: "1",
            tabCards: [
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 0
+                   id: 0,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 1
+                   id: 1,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 2
+                   id: 2,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 3
+                   id: 3,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 4
+                   id: 4,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 5
+                   id: 5,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
            ]
        },
@@ -47,170 +58,182 @@ export const tabContent = [
            idInString: "2",
            tabCards: [
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'Cool table',
                    price: '1000$',
-                   id: 0
+                   id: 0,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 1
+                   id: 1,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 2
+                   id: 2,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 3
+                   id: 3,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 4
+                   id: 4,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
                {
-                   // img: Table,
+                   img: "https://picsum.photos/800/450?random=1",
                    title: 'School desk',
                    price: '1000$',
-                   id: 5
+                   id: 5,
+    authors: [{ name: "Kate Morrison", avatar: "/static/images/avatar/7.jpg" }],
+
                },
            ]
        },
 
-       {
-           id: 3,
-           idInString: "3",
-           tabCards: [
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 0
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 1
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 2
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 3
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 4
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 5
-               },
-           ]
-       },
+//        {
+//            id: 3,
+//            idInString: "3",
+//            tabCards: [
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 0
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 1
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 2
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 3
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 4
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 5
+//                },
+//            ]
+//        },
 
-       {
-           id: 4,
-           idInString: "4",
-           tabCards: [
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 0
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 1
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 2
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 3
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 4
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 5
-               },
-           ]
-       },
+//        {
+//            id: 4,
+//            idInString: "4",
+//            tabCards: [
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 0
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 1
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 2
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 3
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 4
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 5
+//                },
+//            ]
+//        },
 
-       {
-           id: 5,
-           idInString: "5",
-           tabCards: [
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 0
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 1
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 2
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 3
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 4
-               },
-               {
-                   // img: Table,
-                   title: 'School desk',
-                   price: '1000$',
-                   id: 5
-               },
-           ]
-       }
+//        {
+//            id: 5,
+//            idInString: "5",
+//            tabCards: [
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 0
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 1
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 2
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 3
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 4
+//                },
+//                {
+//                    img: "https://picsum.photos/800/450?random=1",
+//                    title: 'School desk',
+//                    price: '1000$',
+//                    id: 5
+//                },
+        //    ]
+    //    }
 ];
